cd72f15c0741f9afc65fed45471d65d5b26b72b9
Gab <91504607+gabryellaro@users.noreply.github.com>
2022-06-04 14:16:22 -0300

    Update Robo.java

--- RoboFruta/src/Robo.java
+++ RoboFruta/src/Robo.java
@@ -16,110 +16,72 @@ public class Robo{
     }
 
 
-    public Robo(int x2, int y2, Object cor2, int i) {
+    public Robo() {
     }
 
     public void inicializarRobo(){
-        
 
+        pos[0][0] = 1;
+    
     }
 
 
     public int mover(){
-        Ambiente ambiente = new Ambiente(x,y, pos);
+
+      
         Scanner input = new Scanner(System.in);
         
         int[][] pos = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
 
-        pos = ambiente.getM();
-
-        
-
         int movimento;
 
-    
-
         movimento = input.nextInt();
+        
+      switch (movimento){
 
+        case 1:
+
+        pos[x++][y]= 1;
       
 
-        /*switch (pos.hashCode()){
-            case "Up".hashCode() : 
-            return pos[x++];/* */
-        
-        
-
-        
-        //pra baixo
-      if(movimento==1){
-        
-
-        for(int[] n:pos ){
-            for(int v:n ){
+        for(int n[]:pos ){
+            for(int v: n ){
                 System.out.printf("%d  " , v );
                 
             }
             System.out.printf("%n");
         }
-      };
-      //pra cima
-      if(movimento==2){
-        for(int l = 0; l<x;l++){
-            for(int c = 0; c<y;c++){
-                pos[l][c] = '0';
-            }
-        }
 
-        for(int l = 0; l<x;l--){
-            for(int c = 0; c<y;c++){
-                System.out.printf("%d  " , pos[l--] );
+
+     break;  
+     
+
+        case 2:
+
+        pos[x--][y]= 1;
+      
+
+        for(int n[]:pos ){
+            for(int v: n ){
+                System.out.printf("%d  " , v );
                 
             }
             System.out.printf("%n");
         }
-      };
-      //pra direita
-      if(movimento==3){
-        for(int l = 0; l<x;l++){
-            for(int c = 0; c<y;c++){
-                pos[l][c] = '0';
-            }
-        }
-
-        for(int l = 0; l<x;l++){
-            for(int c = 0; c<y;c++){
-                System.out.printf("%d  " , pos[c++] );
-                
-            }
-            System.out.printf("%n");
-        }
-      };
-      //pra esquerda
-      if(movimento==4){
-        for(int l = 0; l<x;l++){
-            for(int c = 0; c<y;c++){
-                pos[l][c] = '0';
-            }
-        }
-
-        for(int l = 0; l<x;l++){
-            for(int c = 0; c<y;c++){
-                System.out.printf("%d  " , pos[c--] );
-                
-            }
-            System.out.printf("%n");
-        }
-    
-    
+     break; 
         
+    
+    
+    };
+
+
 
     
-    }return mover();
+    return pos[0][0];}
     
 
    
 
 
 
-}
 }
