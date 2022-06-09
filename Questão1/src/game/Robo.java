3911f4312e44a7cdb6042f3d47a97fca92f009a2
Kalil <106711007+Kal1l@users.noreply.github.com>
2022-06-09 13:33:26 -0300

    Add files via upload

---
+++ Questão1/src/game/Robo.java
@@ -0,0 +1,65 @@
+package game;
+import exception.MovimentoInvalidoException;
+public class Robo {
+    //posições do robô no eixo
+    protected int x=0;
+    protected int y=0;
+    protected String cor; 
+
+    public Robo(String cor){
+        this.cor=cor;
+    }
+
+    public void mover(String movimento) throws MovimentoInvalidoException {;
+        if(movimento.equals("up")){
+            if(x-1<0) throw new MovimentoInvalidoException();
+            x--;
+        }
+        else if(movimento.equals("down")){
+            if(x+1>4) throw new MovimentoInvalidoException();
+            x++;
+        }
+        else if(movimento.equals("right")){
+            if(y+1>4) throw new MovimentoInvalidoException();
+            y++;
+        }
+        else if(movimento.equals("left")){
+            if(y-1<0) throw new MovimentoInvalidoException();
+            y--;
+        }
+    }
+
+        public void mover(int movimento) throws MovimentoInvalidoException{
+        if(movimento==1){
+            if(x-1<0) throw new MovimentoInvalidoException();
+            x--;
+        }
+        else if(movimento==2){
+            if(x+1>4) throw new MovimentoInvalidoException();
+            x++;
+        }
+        else if(movimento==3){
+            if(y+1>4) throw new MovimentoInvalidoException();
+            y++;
+        }
+        else if(movimento==4){
+            if(y-1<0) throw new MovimentoInvalidoException();
+            y--;
+        }
+    }
+
+    //condição pro fim do jogo
+    public boolean comeufruta(int frutaX,int frutaY){
+        if(x==frutaX && y==frutaY){
+            return true;
+        }
+        //else
+        return false;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+}
---
+++ Questão3/src/exception/exception/MovimentoInvalidoException.java
@@ -0,0 +1,7 @@
+package exception;
+public class MovimentoInvalidoException extends Exception{
+    public String toString(){
+        
+        return "Movimento Inválido,tente novamente";
+    }
+}
