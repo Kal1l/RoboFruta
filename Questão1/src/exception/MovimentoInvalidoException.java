4989a16181d9e80f5563020496ef9e15f9db819c
Kalil <106711007+Kal1l@users.noreply.github.com>
2022-06-09 13:34:43 -0300

    Delete Questão1 directory

--- Questão1/src/exception/MovimentoInvalidoException.java
+++
@@ -1,8 +0,0 @@
-package exception;
-
-public class MovimentoInvalidoException extends Exception{
-    public String toString(){
-        
-        return "Movimento Inválido,tente novamente";
-    }
-}
--- Questão1/src/exception/PosiçãoInvalidaException.java
+++
@@ -1,8 +0,0 @@
-package exception;
-
-public class PosiçãoInvalidaException extends Exception{
-    public String toString(){
-        
-        return "Posição da fruta invalida";
-    }
-}
